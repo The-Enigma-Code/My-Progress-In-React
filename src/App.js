4ea8af8ee0650d7025dff520314fdf7b8cf4a425
The-Enigma-Code <niteshchaudhary0812@gmail.com>
2025-04-06 01:15:47 +0530

    Progress  Log-1 6th April 25 useReducer, next React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import L5UseReducerExercises from './MyLearning/L5UseReducerExercises';
 
-function App() {
+
+// import L4UseReducer from './MyLearning/L4UseReducer';
+
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+ <L5UseReducerExercises/>
+
+    </>
   );
-}
+};
 
 export default App;
